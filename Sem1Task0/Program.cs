99eae616641e8f8e42eb9819b7defbf9acd0674d
Nick <nikkashin659@gmai.com>
2023-01-13 22:10:43 +0300

    dd solution for tassk 0,7

---
+++ Sem1Task0/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 1
+// Проверяем является ли первое число квадратом второго
+// Считываем данные с консоли
+string? inputNum1 = Console.ReadLine();
+// Считываем данные с консоли
+string? inputNum2 = Console.ReadLine();
+if (inputNum1!=null && inputNum2!=null)
+{
+   //Парсим введенное число
+int number1 = int.Parse(inputNum);
+//Парсим введенное число
+int number2 = int.Parse(inputNum);
+  
+
+   if(number1 == number2*number2)
+   {
+    Console.WriteLine("Первое число квадрат второго!");
+   }
+   else
+   {
+     Console.WriteLine("Первое число не квадрат второго!");
+
+   }
+
+
+
+?
+
+// //Проверяем, чтобы данные были не пустыми
+// if (inputNum1 != null && inputNum2 != null)
+// {
+//     //Парсим введенное число
+//     int number1 = int.Parse(inputNum1);
+//     //Парсим введенное число
+//     int number2 = int.Parse(inputNum2);
+
+//     // if(number1==number2*number2)
+//     // {
+//     //     Console.WriteLine("Первое число квадрат второго!");
+//     // }
+//     // else
+//     // {
+//     //     Console.WriteLine("Первое число не квадрат второго!");
+//     // }
+
+
+
+// }
---
+++ Sem1Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача №0
+// Программа возводит в квадрат
+string? inputNum = Console.ReadLine();
+// Считываем данные с консоли
+if (inputNum!=null)
+{
+//Парсим введенное число
+int number = int.Parse(inputNum);
+// Находим квадрат числа
+//int result = number*number;
+
+int result = (int)Math.Pow(number, 2);
+
+// Выводим данные на консоль
+Console.WriteLine(result);
+
+//// Console.WriteLine(Math.Pow(int.Parse(inputNum),2));
+
+
+
+}
